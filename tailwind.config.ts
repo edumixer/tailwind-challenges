ca7830c5efc706ab2eb226d3c75e76793cd2f312
edumixer <edu.cena.wf@gmail.com>
2025-03-23 20:48:39 -0300

    chore: base project

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          500: '#ef4444',
+        },
+        white: '#ffffff',
+        gray: {
+          200: '#e5e7eb',
+        },
+        blue: {
+          100: '#dbeafe',
+          400: '#60a5fa',
+          500: '#3b82f6',
+        },
+        green: {
+          100: '#dcfce7',
+        },
+        pink: {
+          100: '#fce7f3',
+        },
+        yellow: {
+          100: '#fef9c3',
+        },
+        purple: {
+          100: '#f3e8ff',
+          500: '#a855f7',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
