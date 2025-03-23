b0e38dd518362932b959390c2ab85e4f26586038
edumixer <edu.cena.wf@gmail.com>
2025-03-23 20:52:39 -0300

    feat(Feedback): add feedback to know when question is correct

---
+++ src/components/Feedback.tsx
@@ -0,0 +1,11 @@
+interface FeedbackProps {
+  message: string;
+  type: "success" | "error";
+}
+
+export default function Feedback({ message, type }: FeedbackProps) {
+  if (!message) return null;
+
+  const color = type === "success" ? "text-green-600" : "text-red-600";
+  return <div className={`mt-4 ${color} font-semibold`}>{message}</div>;
+}
