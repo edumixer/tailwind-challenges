d881fd43f8f9042c3323bd755edd9b64fba63825
edumixer <edu.cena.wf@gmail.com>
2025-03-29 14:57:42 -0300

    feat: create home ui component

---
+++ src/components/ui/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Aprenda Tailwind CSS
+      </h1>
+      <p className="text-xl text-gray-600">
+        Selecione uma categoria no menu lateral para começar!
+      </p>
+    </div>
+  );
+}
